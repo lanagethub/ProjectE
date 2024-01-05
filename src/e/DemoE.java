c7d27c4dfc8e325542a0b5ef03cea0e464650177
mustafaalani <mustafaalani@192.168.0.233>
2024-01-05 13:15:33 -0700

    Initial Commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,13 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Lana");
+		
+		System.out.println("Day5");
+		System.out.println("Day6");
+	}
+
+}
